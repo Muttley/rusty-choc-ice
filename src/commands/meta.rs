4b5fb739545ed158f20a53f27af86a98611a9b33
Paul Maskelyne <muttley@muttleyville.org>
2021-05-28 16:04:02 +0100

    use embed for ping response

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,10 +1,22 @@
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::Colour;
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-	msg.channel_id.say(&ctx.http, "Pong!").await?;
+	let msg = msg.channel_id.send_message(&ctx.http, |m| {
+		m.embed(|e| {
+			e.title("Pong");
+			e.color(Colour::DARK_GREEN);
+			e
+		});
+		m
+	}).await;
+
+	if let Err(why) = msg {
+		println!("Error sending message: {:?}", why);
+	}
 
 	Ok(())
 }
